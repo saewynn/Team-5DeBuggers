407096843782c3670bf9aa49bcf91f0eec102987
gordonsahi <gordonsahi@ecs.vuw.ac.nz>
2016-04-22 16:01:36 +1200

    Networking Libraries from Example

--- Robot.c
+++ Robot.c
@@ -5,6 +5,10 @@ extern "C" int InitHardware();
 extern "C" int SetMotor(int motor, int dir, int speed);
 extern "C" int Sleep(int sec, int usec);
 extern "C" int WriteDigital(int chan, char level);
+extern "C" int init(int d_lev);
+extern "C" int connect_to_server( char server_addr[15],int port);
+extern "C" int send_to_server(char message[24]);
+extern "C" int receive_from_server(char message[24]);
 
 
 int motor1(){
