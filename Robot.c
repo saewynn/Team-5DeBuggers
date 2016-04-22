71ff0f3379311d420b5a716d7ee0fc495228f089
saewynn <ned.wilkinson-dwight@hotmail.com>
2016-04-22 15:58:08 +1200

    Adding Libraries for motor

--- Robot.c
+++ Robot.c
@@ -1,2 +1,15 @@
 #include <stdio.h>
 #include <time.h>
+
+extern "C" int InitHardware();
+extern "C" int SetMotor(int motor, int dir, int speed);
+extern "C" int Sleep(int sec, int usec);
+extern "C" int WriteDigital(int chan, char level);
+
+
+int motor1(){
+  
+  
+  
+  
+}
