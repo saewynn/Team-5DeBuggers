795e54fa41c4232c38ea7415de486909280307bb
gordonsahi <gordonsahi@ecs.vuw.ac.nz>
2016-05-03 13:16:43 +1200

    Adding correct server address and port

--- networking.c
+++ networking.c
@@ -11,7 +11,7 @@ int main (){
    // everything is working correctly
    init(1);
    //connects to server with the ip address 192.168.1.2
-   connect_to_server(192.168.1.2, 22);
+   connect_to_server(130.195.6.196, 1024);
    //sends a message to the connected server
    send_to_server("Hello server");
    //receives message from the connected server
