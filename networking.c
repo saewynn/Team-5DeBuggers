203e97d66d47b78b8d742992eb1f90aa0dd48fa3
gordonsahi <gordonsahi@ecs.vuw.ac.nz>
2016-05-05 12:36:41 +1200

    Reworked code

--- networking.c
+++ networking.c
@@ -7,17 +7,14 @@ extern "C" int send_to_server(char message[24]);
 extern "C" int receive_from_server(char message[24]);
 
 int main (){
-   // This sets up the RPi hardware and ensures
-   // everything is working correctly
+
    init(1);
-   //connects to server with the ip address 192.168.1.2
-   connect_to_server(130.195.6.196, 1024);
-   //sends a message to the connected server
-   send_to_server("Hello server");
-   //receives message from the connected server
-   int message;
-   message=receive_from_server(message);
-   send_to_server("message");
-   
+   connect_to_server("130.195.6.196", 1024);
+   send_to_server("Please");
+   char message[24];
+   receive_from_server(message);
+   printf("%s", message);
+   send_to_server(message);
+
 
 return 0;}
