471249f0c107b97781821fcd17e3e64fa6b1dcd2
e2Eain <eaineiht@myvuw.ac.nz>
2016-05-13 15:32:03 +1200

    Update NewRun.cpp

--- NewRun.cpp
+++ NewRun.cpp
@@ -2,7 +2,6 @@
 #include <time.h>
 #include <string.h>
 
-extern "C" int init_hardware();
 extern "C" int init(int d_lev);
 extern "C" int display_picture(int delay_sec,int delay_usec);
 extern "C" int save_picture(char filename[5]);
@@ -28,16 +27,16 @@ extern "C" int update_screen();
 int main(){
   init(1);
   int x;
-  
+
   connect_to_server("130.195.6.196", 1024); //Opening the gate code
    send_to_server("Please");
    char message[24];
    receive_from_server(message);
    printf("%s", message);
    send_to_server(message);
-   
- 
-  
+
+
+
    for (x = 0; x < 8; x++)
     {
       select_IO(x,0);
@@ -47,10 +46,12 @@ int main(){
        take_picture();
        int sum = 0;
        float kp = 0.5;
-       int ,w ,s;
+       int w;
+       int s;
+       int proportional_signal=0;
        for(int x = 0; x < 320; x++){
             w = get_pixel(x,120,3);
-       if (w>127){s=1;};
+       if(w>127){s=1;}
        else{s=0;};
        sum = sum + (x-160)*s;}
        proportional_signal = sum * kp;
@@ -62,21 +63,21 @@ int main(){
                 set_motor(1, -1*proportional_signal);
                 set_motor(2, proportional_signal);
             }
-            else(){
+            else{
                  set_motor(1, proportional_signal);
                 set_motor(2,  proportional_signal);
             }
        update_screen();
-        
+
        for (x = 0 ; x < 8; x++)
        {
        int adc_reading = read_analog(x);
        printf("ai=%d av=%d\n",x,adc_reading);
        }
      }
-    
+
     set_motor(1,0);
     set_motor(2,0);
-  
+
     return 0;
 }
