d158b9dc578b4cc59a689e119ac091b2c30bd210
e2Eain <eaineiht@myvuw.ac.nz>
2016-05-27 15:23:06 +1200

    Just fixed a few errors in the code using Code blocks

--- T-inter.cpp
+++ T-inter.cpp
@@ -127,3 +127,143 @@ if (left && straight && right){
          }// while
 
             return 0;
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            int main(){
+
+    init(0);
+
+
+    while(1){
+
+       take_picture();
+       int pixelval = 0;
+       int num = 0;
+       int error=0;
+       int s;
+       bool left = false;
+       bool straight = false;
+       bool right = false;
+
+
+       for(int x = 0; x < 320; x++){
+            pixelval = get_pixel(x,120,3);//middle
+            if(pixelval > 127){
+              // pixel white
+              s = 1;
+
+              if ( x <=80){
+                  left = true;
+              }else if(x >= 140 && x <= 200){
+                  straight = true;
+              }else if(x >= 280){
+                  right = true;
+              }
+            }
+
+            else{
+              //pixel black
+                s = 0;
+            }
+            error = error+(x-160)*s;
+            num = num + s;
+         }
+
+         printf("error=%d\n",error);
+         printf("num= %d\n",num);
+if(num<150){
+         if(num>0){
+              // at least one white pixel
+              printf("Forward\n");
+              error=error/num;
+
+              //printf("%d\n", pixelval);
+              //error=error/num;
+              int v1 = -90 + 0.75*error;
+              int v2 = -90 - 0.75*error;
+              printf("v1 = %d v2 = %d\n",v1,v2);
+              set_motor(1,v1);
+              set_motor(2,v2);
+          }else{
+              // no white pixels at all
+              printf("Backing\n");
+              //we need to change this so instead of backing, it will turn around.
+              set_motor(1,0);
+              set_motor(2,90);
+          }
+          Sleep(0,400000);
+
+//may need to write methods about if some are wrong and some are false?? I have no clue.
+}if(num=240){
+printf("T-intersection\n");
+}
+
+
+
+// all we need to do is code it, so that it prioritses turning left > straight > right.
+else if (left && straight && right){
+    //turn left
+}else if(left && straight){
+    //turn left
+}else if(left && right){
+    //turn left
+}else if(left){
+    //turn left
+}else if(straight && right){
+    //go straight
+}else if(straight){
+    //go straight
+}else if(right){
+    //go right
+}
+}
+
+        //while
+
+        return 0;
+}
+
