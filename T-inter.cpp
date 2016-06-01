54f5eddea03d4984af5424607885b37660d216c6
gordonsahi <gordonsahi@ecs.vuw.ac.nz>
2016-06-01 17:46:06 +1200

    Update T-inter.cpp

--- T-inter.cpp
+++ T-inter.cpp
@@ -122,10 +122,10 @@ Sleep(0,500000);
 
 }else if(centerpix>7 && leftpix>7 && rightpix>7 && forwardpix==0){
 printf("TInter\n");
-set_motor(1,-100);
-set_motor(2,100);
+set_motor(1,100);
+set_motor(2,-100);
 
-Sleep(0,500000);
+Sleep(1,0);
 
 
 
