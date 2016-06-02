f3616b2a05932cf8902e90334ca853d9de2c04db
gordonsahi <gordonsahi@ecs.vuw.ac.nz>
2016-06-03 10:49:12 +1200

    Update T-inter.cpp

--- T-inter.cpp
+++ T-inter.cpp
@@ -119,7 +119,7 @@ printf("My process ID : %d\n", getpid());
         printf("Right=%d\n",rightpix);
 
 
-if(centerpix>7 && leftpix>7 && rightpix>7 && forwardpix>25){
+if(centerpix>7 && leftpix>7 && rightpix>7 && forwardpix>30){
 printf("Intersection++++++++++++++++++++++++++++++++++\n");
 set_motor(1,-200);
 set_motor(2,-200);
@@ -128,7 +128,7 @@ Sleep(0,500000);
 
 
 
-}else if(centerpix>7 && leftpix>7 && rightpix>7 && forwardpix<25){
+}else if(centerpix>7 && leftpix>7 && rightpix>7 && forwardpix<30){
 printf("TInter===================================\n");
 set_motor(1,-150);
 set_motor(2,-150);
